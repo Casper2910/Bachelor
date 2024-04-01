0efac46d8ad5736d9ca8912422ce696b1ba0b410
Casper2910 <94049624+Casper2910@users.noreply.github.com>
2024-04-01 22:05:45 +0200

    signing and verification of certificate

--- Signing.py
+++ Signing.py
@@ -1,44 +1,26 @@
-from cryptography.hazmat.primitives.asymmetric import ed25519
-from key_to_bytes import key_to_bytes
+from cryptography.hazmat.primitives.serialization import load_ssh_private_key, load_ssh_public_key
 
+private_key = load_ssh_private_key(
+    b"""-----BEGIN OPENSSH PRIVATE KEY-----
+    b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+    QyNTUxOQAAACCxT0HlJknXPScgNMQM4doq4leJ4yGyPBGBtXNlaWFCtQAAAJh64bC3euGw
+    twAAAAtzc2gtZWQyNTUxOQAAACCxT0HlJknXPScgNMQM4doq4leJ4yGyPBGBtXNlaWFCtQ
+    AAAEDkqygDl+HsGtr/taNxQoZFvfMzNZ95jVHBmYTjqP6jqrFPQeUmSdc9JyA0xAzh2iri
+    V4njIbI8EYG1c2VpYUK1AAAAE2Nhc3BlQEhhcmJvZS1LYXNzZW4BAg==
+    -----END OPENSSH PRIVATE KEY-----""",
+    password=None,)
 
-def sign(message, key):
-    try:
-        # Convert key to bytes
-        key_bytes = key_to_bytes(key)
-        if key_bytes is None:
-            return None
+signature = private_key.sign(b"hello")
 
-        # Check if the private key bytes have the correct length
-        if len(key_bytes) != 32:
-            print("Error: Invalid key length. Ed25519 private key should be 32 bytes long.")
-            return None
+print(signature)
 
-        # Deserialize the private key
-        private_key = ed25519.Ed25519PrivateKey.from_private_bytes(key_bytes)
+# Load the public key
+public_key = load_ssh_public_key(
+    b'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFPQeUmSdc9JyA0xAzh2iriV4njIbI8EYG1c2VpYUK1')
 
-        # Sign the message
-        signature = private_key.sign(message.encode('utf-8'))
-
-        return signature
-    except Exception as e:
-        print("Error:", e)
-        return None
-
-
-# Example usage
-private_key_pem = """
------BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
-QyNTUxOQAAACCxT0HlJknXPScgNMQM4doq4leJ4yGyPBGBtXNlaWFCtQAAAJh64bC3euGw
-twAAAAtzc2gtZWQyNTUxOQAAACCxT0HlJknXPScgNMQM4doq4leJ4yGyPBGBtXNlaWFCtQ
-AAAEDkqygDl+HsGtr/taNxQoZFvfMzNZ95jVHBmYTjqP6jqrFPQeUmSdc9JyA0xAzh2iri
-V4njIbI8EYG1c2VpYUK1AAAAE2Nhc3BlQEhhcmJvZS1LYXNzZW4BAg==
------END OPENSSH PRIVATE KEY-----
-"""
-
-# Use the private key bytes to sign a message
-message = "Hello"
-signature = sign(message, private_key_pem)
-if signature:
-    print("Signature:", signature.hex())
+# Verify the signature
+try:
+    public_key.verify(signature, b"hello")
+    print("Signature verified successfully.")
+except Exception as e:
+    print("Verification failed:", e)
