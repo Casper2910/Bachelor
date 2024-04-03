3dc2bfd77e7906ac9baa9bd8cdc62888c2edc0e5
Casper2910 <94049624+Casper2910@users.noreply.github.com>
2024-04-03 18:15:09 +0200

    signing and verification of certificate

--- Signing.py
+++ Signing.py
@@ -1,16 +1,16 @@
 from cryptography.hazmat.primitives.serialization import load_ssh_private_key, load_ssh_public_key
-
+import multibase
 private_key = load_ssh_private_key(
     b"""-----BEGIN OPENSSH PRIVATE KEY-----
-    b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
-    QyNTUxOQAAACCxT0HlJknXPScgNMQM4doq4leJ4yGyPBGBtXNlaWFCtQAAAJh64bC3euGw
-    twAAAAtzc2gtZWQyNTUxOQAAACCxT0HlJknXPScgNMQM4doq4leJ4yGyPBGBtXNlaWFCtQ
-    AAAEDkqygDl+HsGtr/taNxQoZFvfMzNZ95jVHBmYTjqP6jqrFPQeUmSdc9JyA0xAzh2iri
-    V4njIbI8EYG1c2VpYUK1AAAAE2Nhc3BlQEhhcmJvZS1LYXNzZW4BAg==
-    -----END OPENSSH PRIVATE KEY-----""",
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACCxT0HlJknXPScgNMQM4doq4leJ4yGyPBGBtXNlaWFCtQAAAJh64bC3euGw
+twAAAAtzc2gtZWQyNTUxOQAAACCxT0HlJknXPScgNMQM4doq4leJ4yGyPBGBtXNlaWFCtQ
+AAAEDkqygDl+HsGtr/taNxQoZFvfMzNZ95jVHBmYTjqP6jqrFPQeUmSdc9JyA0xAzh2iri
+V4njIbI8EYG1c2VpYUK1AAAAE2Nhc3BlQEhhcmJvZS1LYXNzZW4BAg==
+-----END OPENSSH PRIVATE KEY-----""",
     password=None,)
 
-signature = private_key.sign(b"hello")
+signature = private_key.sign(b'did:iota:test1')
 
 print(signature)
 
@@ -18,9 +18,16 @@ print(signature)
 public_key = load_ssh_public_key(
     b'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFPQeUmSdc9JyA0xAzh2iriV4njIbI8EYG1c2VpYUK1')
 
+base58 = multibase.encode('base58btc', b'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILFPQeUmSdc9JyA0xAzh2iriV4njIbI8EYG1c2VpYUK1')
+print("base58:", base58)
+decoded = multibase.decode(base58)
+uft8_decoded = decoded.decode('utf-8')
+print(decoded)
+print(uft8_decoded)
+
 # Verify the signature
 try:
-    public_key.verify(signature, b"hello")
+    public_key.verify(signature, b'did:iota:test1')
     print("Signature verified successfully.")
 except Exception as e:
     print("Verification failed:", e)
