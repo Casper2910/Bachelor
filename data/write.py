566ba9e790fdf7618573cbac0cc2a7fce2c9d57f
Casper <casperbramm@hotmail.com>
2024-05-03 17:42:08 +0200

    data handling

--- data/write.py
+++ data/write.py
@@ -5,14 +5,12 @@ def append_to_file(filename, data):
     try:
         # Open the file in append mode, creating it if it doesn't exist
         with open(filename, 'a') as file:
-            # Write the data to the file
-            file.write(data + '\n')  # Assuming each data entry is written on a new line
+            # Convert data to float and then write it to the file
+            file.write(str(float(data)) + '\n')  # Assuming each data entry is written on a new line
         print("Data appended to", filename)
     except FileNotFoundError:
         print("File not found. Creating a new file...")
         # Create a new file and write the data
         with open(filename, 'w') as file:
-            file.write(data + '\n')
+            file.write(str(float(data)) + '\n')
         print("Data written to new file:", filename)
-
-
