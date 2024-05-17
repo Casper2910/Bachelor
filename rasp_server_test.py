ff1d1584663bd9dc896a68afa1cb5d9ae669c5bf
Casper2910 <94049624+Casper2910@users.noreply.github.com>
2024-05-17 09:37:37 +0200

    updates

--- rasp_server_test.py
+++ rasp_server_test.py
@@ -3,6 +3,7 @@ import threading
 import base64
 import json
 import time
+import random
 from queue import Queue
 from keys.Signing import verify_proof
 from block_id_dictonary.write_read_dict import insert_entry, find_id, is_blacklisted, add_to_blacklist
@@ -18,67 +19,98 @@ PORT = 8081  # port
 ISSUER_HOST = '192.168.0.141'  # issuer ip
 ISSUER_PORT = 8080  # issuer port
 
+RETRY_DELAY = 5  # seconds
 
-def handle_device(device_socket, device_address, issuer_socket):
-    while True:
-        try:
-            # Receive data from device
-            received_data = device_socket.recv(1024).decode("utf-8").strip()
-            print('data:', received_data)
+def handle_device(device_socket, device_address):
+    # Receive data from device
+    received_data = device_socket.recv(1024).decode("utf-8").strip()
 
-            data = json.loads(received_data)
+    print('data:', received_data)
 
-            DID = data['DID']
-            temp = data['temperature']
+    data = json.loads(received_data)
 
-            if is_blacklisted(DID):
-                print('DID is blacklisted')
-                continue
+    DID = data['DID']
+    temp = data['temperature']
 
-            if DID not in id_dict.values():
-                print('New DID')
-                # Save pending request locally
-                with open("pending_requests.json", "a") as f:
-                    f.write(json.dumps(data) + '\n')
+    if is_blacklisted(DID):
+        print('DID is blacklisted')
+        pass
+
+    if DID not in id_dict.values():
+        print('New DID')
+        # request did doc:
+        send_json(69420, device_address[0], 8082)  # Arduino's is listening on port 8082
+
+        # Wait for the did document
+        while True:
+            print('Awaiting did document from device...')
+            # Receive data from the device
+            received_data = device_socket.recv(1024).decode("utf-8")
+
+            # Parse the received JSON string
+            received_json = json.loads(received_data)
+
+            if "proof" in received_json:
+                DID_doc = received_json
+                # Do something if the field and its content match
+                print('DID doc:', DID_doc)
+                break
+
+        # Attempt to connect to the issuer for authentication
+        issuer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while True:
+            try:
+                print('Attempting to connect to issuer...')
+                issuer_socket.connect((ISSUER_HOST, ISSUER_PORT))
+                break
+            except Exception as e:
+                print('error: ', e)
+                print('Issuer is down. Retrying in', RETRY_DELAY, 'seconds...')
+                time.sleep(RETRY_DELAY)
+
+        # Send the DID to the issuer for authentication
+        print('Sending DID doc to issuer')
+        issuer_socket.send(DID.encode("utf-8"))
+
+        while True:
+            print('Awaiting issuers signature...')
+            # Receive data from the issuer
+            proof = issuer_socket.recv(1024)
+
+            # Example: Check if the received data from the issuer matches a specific condition
+            if proof == 'no':
+                # add to blacklist
+                print('DID has been blacklisted')
+                add_to_blacklist(DID)
+                break
             else:
-                print('Known DID')
-                block_id = find_id(DID)
-                DID_document = retrieve_block_data(block_id)
-                proof_base64 = DID_document['proof']
-                proof_binary = base64.b64decode(proof_base64)
+                print('DID has been authorized')
+                DID_doc['proof'] = proof
+                DID_doc['publicKey'] = public_key
+                block_id = upload_block(DID_doc)
+                insert_entry(block_id, DID)
+                break
 
-                if verify_proof(proof_binary, public_key, DID):
-                    print(f'Arduino with {DID} is verified')
-                    append_to_file(DID, temp)
-                else:
-                    print(f'Arduino with {DID} is NOT verified')
+    if DID in id_dict.values():
+        print('Known DID')
+        block_id = find_id(DID)
+        DID_document = retrieve_block_data(block_id)
+        proof_base64 = DID_document['proof']
+        proof_binary = base64.b64decode(proof_base64)
 
-        except Exception as e:
-            print(e)
+        if verify_proof(proof_binary, public_key, DID):
+            print(f'Arduino with {DID} is verified')
 
-
-def handle_issuer(device_queue, issuer_socket):
-    while True:
-        # Check if there are pending requests
-        if not device_queue.empty():
-            # Accept connection from device
-            device_conn, device_address = device_queue.get()
-            print("Connection from device:", device_address)
-
-            # Start handling device in a separate thread
-            device_thread = threading.Thread(target=handle_device, args=(device_conn, device_address, issuer_socket))
-            device_thread.start()
+            # continue to document for 100 cycles:
+            for i in range(100):
+                append_to_file(DID, temp)
         else:
-            # Sleep for a while before checking again
-            time.sleep(1)
+            print(f'Arduino with {DID} is NOT verified')
 
 
 def main():
     # Create a socket object for device
     device_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Create a socket object for issuer
-    issuer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Display ip and port for the Arduino config
     print(f"Device socket listening for connections on {HOST, PORT}...")
@@ -89,31 +121,14 @@ def main():
     # Listen for incoming connections on the device socket
     device_socket.listen(5)
 
-    # Create a queue to hold incoming device connections
-    device_queue = Queue()
-
-    # Start the issuer handler thread
-    issuer_handler_thread = threading.Thread(target=handle_issuer, args=(device_queue, issuer_socket))
-    issuer_handler_thread.start()
-
-    while True:
-        try:
-            # Connect to the issuer
-            issuer_socket.connect((ISSUER_HOST, ISSUER_PORT))
-            print("Connected to the issuer.")
-            break  # Exit the loop if connection successful
-
-        except:
-            print("Issuer is currently unreachable. Pending requests will be saved locally.")
-            break  # Exit the loop if connection refused
-
     while True:
         # Accept connection from device
         device_conn, device_address = device_socket.accept()
         print("Connection from device:", device_address)
 
-        # Add device socket to the queue
-        device_queue.put((device_conn, device_address))
+        # Start a thread to handle the device connection
+        device_thread = threading.Thread(target=handle_device, args=(device_conn, device_address))
+        device_thread.start()
 
 
 if __name__ == "__main__":
