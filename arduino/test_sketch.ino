d337a8497d969fe740717e5bfbd9bfdd4f439188
Casper2910 <94049624+Casper2910@users.noreply.github.com>
2024-05-21 17:45:10 +0200

    testing setup

---
+++ arduino/test_sketch.ino
@@ -0,0 +1,190 @@
+#include <WiFiNINA.h>
+#include <ArduinoJson.h>
+#include <Arduino_MKRIoTCarrier.h>
+#include <ArduinoUniqueID.h>
+
+char ssid[] = "wifi";       // your network SSID (name)
+char pass[] = "pass";    // your network password
+int status = WL_IDLE_STATUS;    // the WiFi status
+
+IPAddress server(192, 168, 0, 155); // IP address of the target device
+int serverPort = 8081;              // port of the target device
+int listenPort = 8082;              // port to listen for incoming requests
+
+WiFiClient client;
+WiFiServer wifiServer(listenPort);  // Define WiFiServer object to listen on port 8082
+
+MKRIoTCarrier carrier;
+
+String DID;                // Variable for DID
+String DeviceID;           // Variable for unique identifier for device
+String DID_Document;       // Variable to store the DID document as a JSON string
+
+/*
+This sketch is a test variant of the original to test performance of the DID and DID document generation.
+*/
+
+unsigned long s1; // creating did
+unsigned long e1;
+
+unsigned long s2; // creating did document
+unsigned long e2;
+
+unsigned long s3; // serialize did json
+unsigned long e3;
+
+unsigned long s4; // serialize did document
+unsigned long e4;
+
+unsigned long time2 // e2 - s2
+unsigned long time4 // e4 - s4
+
+
+void setup() {
+  Serial.begin(9600);
+
+  // Retry carrier initialization until it succeeds
+  while (!carrier.begin()) {
+    Serial.println("Failed to initialize MKRIoTCarrier. Retrying...");
+    delay(1000); // Wait before retrying
+  }
+
+  connectToWiFi(); // Connect to WiFi network
+
+  // Start listening for incoming connections
+  wifiServer.begin();
+
+  Serial.println("Setup complete");
+}
+
+void loop() {
+
+  s1 = millis();
+  // Generate DID
+  getDeviceID();
+  DID = "did:iota:test" + DeviceID;
+  e1 = millis();
+
+  // Generate DID Document
+  generateDIDDocument();
+
+  if (client.connected()) {
+    Serial.println("CONNECTED");
+
+
+    // Create a JSON object for sending data
+    StaticJsonDocument<100> jsonDID;
+    jsonDID["DID"] = DID;
+    float temp = carrier.Env.readTemperature();
+    jsonDID["temperature"] = temp;
+
+    // measure time to serialize JSON object to a string
+    s3 = millis();
+    String jsonData;
+    serializeJson(jsonDID, jsonData);
+    e3 = millis();
+
+    // serialize and send
+    jsonDID["did_time"] = e1 - s1;
+    jsonDID["did_doc_time"] = time2;
+    jsonDID["serializeDID"] = e3 - s3;
+    jsonDID["serializeDID_doc"] = time4;
+    serializeJson(jsonDID, jsonData);
+
+    Serial.println("jsonData: " + jsonData);
+
+    // Send JSON string to server
+    client.print(jsonData);
+
+    // Check if there is any incoming request from the server
+    WiFiClient serverClient = wifiServer.available();
+    if (serverClient) {
+      int request = serverClient.parseInt(); // Read the request as an integer
+      Serial.println("Request received: " + String(request));
+
+      if (request == 69420) {
+        Serial.println("Received request for DID document");
+        sendDIDDocument(); // Send the DID document to the server
+      }
+
+      serverClient.stop(); // Close the connection with the server
+    }
+
+    delay(2000); // Wait before sending next data
+  } else {
+    Serial.println("NOT CONNECTED");
+    if (!connectToServer()) {
+      Serial.println("Connection lost. Reconnecting...");
+      delay(5000); // Wait before attempting to reconnect
+    }
+  }
+}
+
+void connectToWiFi() {
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+  status = WiFi.begin(ssid, pass);
+  int attempts = 0;
+  while (status != WL_CONNECTED && attempts < 5) {
+    Serial.println("Connection attempt failed. Retrying...");
+    delay(5000);  // Wait before retrying
+    status = WiFi.begin(ssid, pass);
+    attempts++;
+  }
+
+  if (status == WL_CONNECTED) {
+    Serial.println("Connected to WiFi");
+  } else {
+    Serial.println("Failed to connect to WiFi");
+    // You might want to implement a mechanism to handle failure, e.g., enter a sleep mode.
+  }
+}
+
+bool connectToServer() {
+  if (client.connect(server, serverPort)) {
+    Serial.println("Connected to server");
+    return true;
+  } else {
+    Serial.println("Connection to server failed");
+    return false;
+  }
+}
+
+void getDeviceID() {
+  UniqueIDdump(Serial);
+  for (size_t i = 0; i < UniqueIDsize; i++) {
+    DeviceID += String(UniqueID[i]);
+  }
+}
+
+void generateDIDDocument() {
+  // Create a JSON object for DID document
+  s2 = millis();
+  StaticJsonDocument<200> jsonDocument;
+  jsonDocument["@context"] = "https://w3id.org/security/suites/ed25519-2020/v1";
+  jsonDocument["id"] = DID;
+  jsonDocument["controller"] = "did:iota:root";
+  jsonDocument["serial"] = DeviceID;
+  jsonDocument["proof"] = "NEEDS_PROOF";
+
+  // Create a nested JSON object for verification method
+  JsonObject verificationMethod = jsonDocument.createNestedObject("verificationMethod");
+  verificationMethod["id"] = "did:iota:test1/path";
+  verificationMethod["type"] = "Ed25519VerificationKey2018";
+  verificationMethod["controller"] = "did:iota:root";
+  verificationMethod["publicKey"] = ""; // Add the actual public key if available
+  e2 = millis();
+  time2 = e2 - s2;
+
+  s4 = millis();
+  // Serialize JSON object to a string
+  serializeJson(jsonDocument, DID_Document);
+  e4 = millis();
+  time4 = e4 - s4;
+}
+
+void sendDIDDocument() {
+  // Send the DID document to the server
+  client.print(DID_Document);
+  Serial.println("Sent DID document to server: " + DID_Document);
+}
---
+++ rasp_server_test.py
@@ -0,0 +1,175 @@
+import socket
+import threading
+import base64
+import json
+import time
+from keys.Signing import verify_proof
+from block_id_dictonary.write_read_dict import insert_entry, find_id, is_blacklisted, add_to_blacklist
+from iota.block_handler import upload_block, retrieve_block_data
+from keys.keys import public_key
+from data.write import append_to_file
+from connection.connector import send_json, obtain_ip
+from id_dict import id_dict
+import csv
+
+HOST = obtain_ip()  # own ip
+PORT = 8081  # port
+
+ISSUER_HOST = '192.168.0.141'  # issuer ip
+ISSUER_PORT = 8080  # issuer port
+
+RETRY_DELAY = 5  # seconds
+
+'''
+This code is an adjustment of the rasp_server.py to test the perfomance of each action measured in time
+
+'''
+
+
+def document_data(file_name, data):
+    try:
+        # Open the CSV file in append mode
+        with open(file_name, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            # Write the data to the CSV file
+            writer.writerow([data])
+    except Exception as e:
+        print(e)
+
+
+def connect_to_issuer():
+    try:
+        print('Attempting to connect to issuer...')
+        issuer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        issuer_socket.connect((ISSUER_HOST, ISSUER_PORT))
+        print('Successfully connected to the issuer.')
+        return issuer_socket
+    except Exception as e:
+        print('Error occurred while connecting to the issuer:', e)
+        return None
+
+
+def handle_device(device_socket, device_address):
+    while True:
+        try:
+            # Receive data from device
+            received_data = device_socket.recv(1024).decode("utf-8").strip()
+            if not received_data:
+                print("No data received. Closing connection.")
+                break
+            print('data:', received_data)
+
+            data = json.loads(received_data)
+            DID = data['DID']
+            temp = data['temperature']
+            document_data("test/did.csv", data['did_time'])
+            document_data("test/did_doc.csv", data['did_time'])
+            document_data("test/serializeDID.csv", data['serializeDID'])
+            document_data("test/serializeDID_doc.csv", data['serializeDID_doc'])
+
+            if is_blacklisted(DID):
+                print('DID is blacklisted')
+                break
+
+            if not DID.startswith('did:iota:test'):
+                print('Does not contain relevant did')
+                break
+
+            if DID not in id_dict.values():
+                print('New DID')
+                # request did doc:
+                send_json(69420, device_address[0], 8082)  # Arduino's is listening on port 8082
+
+                # Wait for the did document
+                while True:
+                    print('Awaiting did document from device...')
+                    # Receive data from the device
+                    received_data = device_socket.recv(1024).decode("utf-8")
+                    if not received_data:
+                        print("No data received for DID document. Retrying...")
+                        continue
+
+                    # Parse the received JSON string
+                    received_json = json.loads(received_data)
+
+                    if "proof" in received_json:
+                        DID_doc = received_json
+                        print('DID doc:', DID_doc)
+                        break
+
+                # Attempt to connect to the issuer for authentication and send the DID to the issuer for authentication
+                while True:
+                    issuer_socket = connect_to_issuer()
+                    if issuer_socket is None:
+                        time.sleep(RETRY_DELAY)
+                    else:
+                        break
+
+                print('Sending DID doc to issuer')
+                issuer_socket.send(DID.encode("utf-8"))
+
+                while True:
+                    print('Awaiting issuer\'s signature...')
+                    # Receive data from the issuer
+                    proof = issuer_socket.recv(1024)
+
+                    if proof == b'no':
+                        print('DID has been blacklisted')
+                        add_to_blacklist(DID)
+                        break
+                    else:
+                        print('DID has been authorized')
+                        DID_doc['proof'] = proof
+                        DID_doc['publicKey'] = public_key
+                        block_id = upload_block(DID_doc)
+                        insert_entry(block_id, DID)
+                        id_dict[block_id] = DID
+                        break
+
+            if DID in id_dict.values():
+                print('Known DID')
+                block_id = find_id(DID)
+                DID_document = retrieve_block_data(block_id)
+                proof_base64 = DID_document['proof']
+                proof_binary = base64.b64decode(proof_base64)
+
+                if verify_proof(proof_binary, public_key, DID):
+                    print(f'Arduino with {DID} is verified')
+                    append_to_file(DID, temp)
+                else:
+                    print(f'Arduino with {DID} is NOT verified')
+                    break
+
+        except (socket.error, json.JSONDecodeError) as e:
+            print(f"Error: {e}. Restarting connection handling.")
+            break
+
+    device_socket.close()
+    print(f"Connection with {device_address} closed.")
+
+
+def main():
+    # Create a socket object for device
+    device_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Display ip and port for the Arduino config
+    print(f"Device socket listening for connections on {HOST, PORT}...")
+
+    # Bind the device socket to a specific address and port
+    device_socket.bind((HOST, PORT))
+
+    # Listen for incoming connections on the device socket
+    device_socket.listen(5)
+
+    while True:
+        # Accept connection from device
+        device_conn, device_address = device_socket.accept()
+        print("Connection from device:", device_address)
+
+        # Start a thread to handle the device connection
+        device_thread = threading.Thread(target=handle_device, args=(device_conn, device_address))
+        device_thread.start()
+
+
+if __name__ == "__main__":
+    main()
